5bd1024fa955230d33c4979892923837c5632c8a
ronm <ronmaca1@gmail.com>
2018-11-13 12:47:17 -0600

    preliminary commit

--- itsy_bitsy_8_channel_logger.ino
+++ itsy_bitsy_8_channel_logger.ino
@@ -1,4 +1,4 @@
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+// Date and time functions using a PCF8523 RTC connected via I2C and Wire lib
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
